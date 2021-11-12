afc45aae33cbdd65d4faab89cd61e35cc7a4acf9
Samandar Turdiev <firsturdiev@gmail.com>
2021-11-12 18:48:51 +0500

    dd

--- src/screens/FeedbackDetails/FeedbackDetails.js
+++ src/screens/FeedbackDetails/FeedbackDetails.js
@@ -1,15 +1,25 @@
-// eslint-disable-next-line no-unused-vars
-import Button from '../../components/Button/Button';
 import { Link } from 'react-router-dom';
 import './FeedbackDetails.css';
+import Feedback from '../../components/Feedback/Feedback';
+import Comment from '../../components/Comment/Comment';
 
 function FeedbackDetails() {
+  const feedback = JSON.parse(localStorage.getItem('feedbacks'))[0];
+
   return (
     <main className="feedback-details site-content">
       <header className="feedback-details__header">
         <Link to="/" className="feedback-details__back-btn btn btn--back btn--back--light">Go Back</Link>
         <Link to="/feedback/edit" className="feedback-details__edit-btn btn btn--blue-orchid">Edit Feedback</Link>
       </header>
+
+      <section className="feedback-details__comments comments">
+        <Feedback className="feedback-details__feedback" {...feedback} />
+
+        <div className="comments__wrapper">
+          <Comment />
+        </div>
+      </section>
     </main>
   )
 }
